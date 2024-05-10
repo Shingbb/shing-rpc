f7f04fdab8b8797fb94bde3d75a738cf7bc1e35b
LBC <541014133@qq.com>
2024-05-10 16:03:51 +0800

    perf: 将序列化工厂改成了使用单例双检索实现了懒加载

--- shing-rpc-core/src/main/java/com/shing/shingrpc/serializer/SerializerFactory.java
+++ shing-rpc-core/src/main/java/com/shing/shingrpc/serializer/SerializerFactory.java
@@ -9,14 +9,17 @@ import com.shing.shingrpc.spi.SpiLoader;
  */
 public class SerializerFactory {
 
-    static {
+    /*static {
         SpiLoader.load(Serializer.class);
-    }
+    }*/
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
+    private static Serializer DEFAULT_SERIALIZER;
+
+//    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
+
 
     /**
      * 获取实例
@@ -25,6 +28,14 @@ public class SerializerFactory {
      * @return
      */
     public static Serializer getInstance(String key) {
+        if (DEFAULT_SERIALIZER == null) {
+            synchronized (SerializerFactory.class) {
+                if (DEFAULT_SERIALIZER == null) {
+                    SpiLoader.load(Serializer.class);
+                    DEFAULT_SERIALIZER = new JdkSerializer();
+                }
+            }
+        }
         return SpiLoader.getInstance(Serializer.class, key);
     }
 
