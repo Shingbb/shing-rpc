032189c0ef69670f8bbb524a4f868d677560c1eb
LBC <541014133@qq.com>
2024-05-23 17:30:45 +0800

    feat: 应用负载均衡器

--- shing-rpc-core/src/main/java/com/shing/shingrpc/proxy/ServiceProxy.java
+++ shing-rpc-core/src/main/java/com/shing/shingrpc/proxy/ServiceProxy.java
@@ -1,58 +1,47 @@
 package com.shing.shingrpc.proxy;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import com.shing.shingrpc.RpcApplication;
 import com.shing.shingrpc.config.RpcConfig;
 import com.shing.shingrpc.constant.RpcConstant;
+import com.shing.shingrpc.loadbalancer.LoadBalancer;
+import com.shing.shingrpc.loadbalancer.LoadBalancerFactory;
 import com.shing.shingrpc.model.RpcRequest;
 import com.shing.shingrpc.model.RpcResponse;
 import com.shing.shingrpc.model.ServiceMetaInfo;
-import com.shing.shingrpc.protocol.*;
 import com.shing.shingrpc.registry.Registry;
 import com.shing.shingrpc.registry.RegistryFactory;
-import com.shing.shingrpc.serializer.JdkSerializer;
 import com.shing.shingrpc.serializer.Serializer;
 import com.shing.shingrpc.serializer.SerializerFactory;
 import com.shing.shingrpc.server.tcp.VertxTcpClient;
-import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.core.net.NetClient;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ServiceLoader;
-import java.util.concurrent.CompletableFuture;
 
 /**
- * 服务代理（JDK 动态代理）
+ * 服务代理（JDK 动态代理）类，用于动态生成目标服务的代理对象，实现远程过程调用。
  *
  * @author shing
  */
 public class ServiceProxy implements InvocationHandler {
 
     /**
-     * 调用代理
+     * 当调用代理对象的方法时，实际执行的逻辑在此方法中实现。
      *
-     * @return
-     * @throws Throwable
+     * @param proxy  代理对象本身
+     * @param method 被调用的方法
+     * @param args   方法调用时传入的参数
+     * @return 调用方法的返回结果
+     * @throws Throwable 方法执行中抛出的异常
      */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        // 指定序列化器
-        /*Serializer serializer = new JdkSerializer();
-        Serializer serializer = null;
-        ServiceLoader<Serializer> serviceLoader = ServiceLoader.load(Serializer.class);
-        for (Serializer service : serviceLoader) {
-            serializer = service;
-        }*/
+        // 使用序列化器
         final Serializer serializer = SerializerFactory.getInstance(RpcApplication.getRpcConfig().getSerializer());
 
-        // 构造请求
+        // 构建 RPC 请求
         String serviceName = method.getDeclaringClass().getName();
         RpcRequest rpcRequest = RpcRequest.builder()
                 .serviceName(serviceName)
@@ -60,19 +49,30 @@ public class ServiceProxy implements InvocationHandler {
                 .parameterTypes(method.getParameterTypes())
                 .args(args)
                 .build();
-        // 从注册中心获取服务提供者请求地址
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
-        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-        serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
-        List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
-        if (CollUtil.isEmpty(serviceMetaInfoList)) {
-            throw new RuntimeException("暂无服务地址");
+
+        try {
+            // 从注册中心获取服务提供者的地址
+            RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+            Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
+            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+            serviceMetaInfo.setServiceName(serviceName);
+            serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+            List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
+            if (CollUtil.isEmpty(serviceMetaInfoList)) {
+                throw new RuntimeException("暂无服务地址");
+            }
+
+            // 使用负载均衡策略选择一个服务提供者
+            LoadBalancer loadBalancer = LoadBalancerFactory.getInstance(rpcConfig.getLoadBalancer());
+            HashMap<Object, Object> requestParams = new HashMap<>();
+            requestParams.put("methodName", rpcRequest.getMethodName());
+            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
+
+            // 发送 TCP 请求并获取响应
+            RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
+            return rpcResponse.getData();
+        } catch (Exception e) {
+            throw new RuntimeException("调用失败");
         }
-        ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
-        // 发送 TCP 请求
-        RpcResponse rpcResponse = VertxTcpClient.doRequest(rpcRequest, selectedServiceMetaInfo);
-        return rpcResponse.getData();
     }
 }
